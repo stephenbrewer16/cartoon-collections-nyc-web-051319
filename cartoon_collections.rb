8578c4a72db170dbf91d0ea99e6f30a933fc4b7a
Stephen Brewer <stephenbrewer16@gmial.com>
2019-05-02 17:04:47 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  counter = 1
+  array.each do |name|
+    puts "#{counter}. #{name}"
+    counter += 1
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |name| name.capitalize + "!"
+end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.include?(caller.length > 4)
+  array.any? do |caller|
+    caller.length > 4
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |snack|
+    cheese_types.include?(snack)
+end
 end
